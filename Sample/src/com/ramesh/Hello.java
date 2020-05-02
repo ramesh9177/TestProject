1d2d05a93a90899f509b00c1e45cd6f51d4c9479
Chennu <52832335+ramesh9177@users.noreply.github.com>
2020-05-02 03:42:04 -0500

    Update Hello.java
    Modified at HitHub

--- Sample/src/com/ramesh/Hello.java
+++ Sample/src/com/ramesh/Hello.java
@@ -5,6 +5,7 @@ public class Hello
 public static void main(String args[])
 {
 	System.out.println("this is my 1st java programme in eclipse");
+	System.out.println("Changs made at Git Hub");
 }
 	
 }

bbacc9937c66e80bd44066dbb5ded9b11e2b2dec
RameshChennu <RameshChennu@LIT-IA-045>
2020-05-02 04:04:24 -0500

    For loop check -in

---
+++ Sample/src/com/ramesh/ForLoop.java
@@ -0,0 +1,17 @@
+package com.ramesh;
+
+public class ForLoop {
+
+
+	public static void main(String[] args) 
+	{
+	    int i;
+		for( i=0;i<10;i++)
+		{			
+			System.out.println("My name is Ramesh Chennu "+i+" time");
+		}
+		
+		System.out.println("Ramesh is now outside of the loop after " +i + " time(s)");
+	}
+
+}

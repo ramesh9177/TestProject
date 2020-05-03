d530408a6f61108433f135e3a01c0d822b052f5a
RameshChennu <RameshChennu@LIT-IA-045>
2020-05-03 06:24:32 -0500

    Test

---
+++ Sample/src/com/test/Test.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import com.ramesh.*;
+
+public class Test 
+{
+
+	public static void main(String[] args) 
+	{
+	
+		AccessModifierAddition ama=new AccessModifierAddition();
+		System.out.println("Addition of two numbers with public access modifier is "+ ama.addTwonumbers(12,13));
+
+	}
+
+}
